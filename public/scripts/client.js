84a9c01c7cff8245ba12837ce3df2cb10324cce5
Sophie <sophiemargueritedubois@gmail.com>
2020-06-18 19:34:09 +0000

    Major clean up and refactor. Remove unecessary picture. Move composer-char-count into client.js file. Store dom elements in variables instead of using jQuery every time I need to access them. Added meaningful comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,3 +1,4 @@
+// Create a tweet article with the data from the input tweet object
 const createTweetElement = tweetObj => {
   return `<article class="tweet">
   <header>
@@ -17,79 +18,120 @@ const createTweetElement = tweetObj => {
 </article>`;
 };
 
+// Escape the string to avoid XSS attacks
 const escape = function(str) {
   let div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 };
 
+// Takes in an array of tweets and prepends them all to the tweet section container
 const renderTweets = tweetObjArr => {
   const section = $('.tweet-section');
+  // Make sure to empty the container before prepending all the tweets to avoid duplicates
   section.empty();
   tweetObjArr.forEach(tweetObj => {
     section.prepend(tweetObj);
   })
 };
 
+// Makes ajax GET request to fetch the existing tweets from the 'database'
 const loadTweets = function() {
   $.ajax('/tweets', { type: 'GET' })
     .then((data) => {
+      // Map each tweetObject to a template literal html article
       const tweetObjArray = data.map( tweet => createTweetElement(tweet));
+      // Add them to the html
       renderTweets(tweetObjArray);
     })
     .catch(() => {
-      console.log('an error occured');
+      console.log('An error occured while trying to fetch the tweets');
     })
 };
 
+// Make sure DOM has loaded before trying to select HTML elements and adding event listener
 $(document).ready(() => {
+  // Gabbing the DOM elements that will be used in the event handlers
+  const textArea = $('.tweet-text-area');
+  const counter = $('.counter');
+  const form = $('form');
+  const errorDiv = $('.error-div');
+  const compose = $('.compose');
+  const tweetForm = $('.new-tweet');
+  const scrollBtn = $('.back-to-top');
   
+  // triggers an ajax fetch on page load to display tweets
   loadTweets();
 
-  $('form').on('submit', function(event) {
+  // Event handler for keyup in text area. 
+  // Updates the character count
+  // Adds class 'under-zero' whe more than 140 chars are typed, which triggers a style change in the css
+  textArea.on('keyup', function() {
+    const numChars = 140 - this.value.length;
+    counter.html(numChars);
+    if (numChars < 0) {
+      counter.addClass('under-zero');
+    } else {
+      counter.removeClass('under-zero');
+    }
+  });
+
+  // Event handler for new-tweet form submission
+  form.on('submit', function(event) {
+    // Prevent default form submission
     event.preventDefault();
-   
-    if (!$('#tweet-text').val().length || $('#tweet-text').val().length > 140) {
-      $('.error-div').empty();
-      $('.error-div').append(`<p><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> ${$('#tweet-text').val().length > 140 ? 'Error: Please spect the 140 character limit' : 'Error: Tweet can not be empty'} <i class="fa fa-exclamation-triangle" aria-hidden="true"></i></p>`)
-      $('.error-div').slideDown('slow');
+    // Form submission validation: textarea value cannot have 0 or over 140 chars
+    if (!textArea.val().length || textArea.val().length > 140) {
+      errorDiv.empty();
+      errorDiv.append(`<p><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> ${$('#tweet-text').val().length > 140 ? 'Error: Please spect the 140 character limit' : 'Error: Tweet can not be empty'} <i class="fa fa-exclamation-triangle" aria-hidden="true"></i></p>`)
+      errorDiv.slideDown('slow');
       return;
     } else {
-      $('.error-div').slideUp('slow');
-      $('.error-div').empty();
+      errorDiv.slideUp('slow');
+      errorDiv.empty();
     };
-
+    // serialize the data before sending it to server
     const serializedTweet = $(this).serialize();
-
+    // makse the ajax post request with serialized data
     $.ajax('/tweets', { type: 'POST', data: serializedTweet })
       .then(() => {
-        $('.counter').html(140);
+        // Reset counter to 0. 
+        // Reset text area to empty
+        // Reload tweets which now includes our new tweet.
+        counter.html(140);
+        textArea.val('');
         loadTweets();
-        $('#tweet-text').val('');
       })
       .catch(() => {
-        console.log('something went wrong..');
+        // Log error to console if error occurs during post request
+        console.log('An error occured while attempting to post the new tweet');
       });
   });
 
-  $('.compose').on('click', function(event) {
-    if ($('.new-tweet').css('display') === 'none') {
-      $('.new-tweet').slideDown('slow');
-      $('#tweet-text').focus();
+  // Event handler for the compose new tweet button
+  compose.on('click', function(event) {
+    // If new-tweet form is hidden, show it, else hide it
+    if (tweetForm.css('display') === 'none') {
+      tweetForm.slideDown('slow');
+      textArea.focus();
     } else {
-      $('.new-tweet').slideUp('slow');
+      tweetForm.slideUp('slow');
     }
   });
 
+  // Event handler for scroll
   $(document).on('scroll', (event) => {
+    // If we are at top of the window, hide the 'back to top' button
+    // Else, show it.
     if ($(window).scrollTop() === 0) {
-      $('.back-to-top').css('display', 'none');
+      scrollBtn.css('display', 'none');
     } else {
-      $('.back-to-top').css('display', 'block');
+      scrollBtn.css('display', 'block');
     }
   });
 
-  $('.back-to-top').on('click', () => {
+  // Event handler for click on 'back to top button'
+  scrollBtn.on('click', () => {
     $(window).scrollTop(0);
   });
   
